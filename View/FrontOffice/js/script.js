eb391ba719388b889e6ec3a73e2b311443e12a16
Oumaima375 <chebbi.oumaima1@esprit.tn>
2024-12-18 07:31:18 +0100

    Update script.js

--- View/FrontOffice/js/script.js
+++ View/FrontOffice/js/script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyDXUD4Ut4yGsHS01l_tdW9cfma1RzJEPx8";
+const API_KEY = "api_key";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 // Initialize user message and file data
